35a990f81296795815ec303e0659832897a05e1b
v-kaushal <v-ksobbana@microsoft.com>
2024-04-17 10:41:26 +0530

    Added python 3.12 support

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "License :: Other/Proprietary License",
