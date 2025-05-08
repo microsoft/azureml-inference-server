3d7c1474325f374398eb1b060430e88fd46a6aa6
Tarun Chevula (CSI INTERFUSION INC) <v-tchevula@microsoft.com>
2025-05-08 10:20:39 +0530

    Update tracing and logging

--- azureml_inference_server_http/server/appinsights_client.py
+++ azureml_inference_server_http/server/appinsights_client.py
@@ -8,15 +8,14 @@ import os
 import time
 
 import flask
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from azure.monitor.opentelemetry.exporter import AzureMonitorLogExporter, AzureMonitorTraceExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.trace import get_tracer_provider, set_tracer_provider, SpanKind
-from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk._logs import LoggingHandler, LoggerProvider
-from opentelemetry._logs import set_logger_provider
+from opentelemetry._logs import set_logger_provider, get_logger_provider
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
-from azure.monitor.opentelemetry.exporter import AzureMonitorLogExporter, AzureMonitorTraceExporter
 from opentelemetry.sdk.trace.sampling import ALWAYS_ON
 
 from .config import config
@@ -43,40 +42,35 @@ class AppInsightsClient(object):
 
         if config.app_insights_enabled and config.app_insights_key:
             try:
-                connection_string = f"InstrumentationKey={config.app_insights_key.get_secret_value()}"
+                instrumentation_key = config.app_insights_key.get_secret_value()
 
-                # Configure the resource
-                resource = Resource.create(
-                    {
-                        "service.name": config.service_name,
-                        "service.instance.id": config.hostname,
-                        "service.version": config.service_version,
-                    }
-                )
+                resource = Resource.create(attributes={"service.name": config.service_name})
 
-                # Configure logging
                 logger_provider = LoggerProvider(resource=resource)
                 set_logger_provider(logger_provider)
-                azure_log_exporter = AzureMonitorLogExporter(connection_string=connection_string)
-                logger_provider.add_log_record_processor(BatchLogRecordProcessor(azure_log_exporter))
+                log_exporter = AzureMonitorLogExporter(
+                    connection_string=f"InstrumentationKey={instrumentation_key}"
+                )
+                get_logger_provider().add_log_record_processor(BatchLogRecordProcessor(log_exporter))
 
-                # Attach the LoggingHandler to the logger
+                # Add log handler
                 self.azureLogHandler = LoggingHandler(level=logging.INFO)
                 logger.addHandler(self.azureLogHandler)
                 logging.getLogger("azmlinfsrv.print").addHandler(self.azureLogHandler)
 
-                # Configure tracing
-                tracer_provider = TracerProvider(resource=resource, sampler=ALWAYS_ON)
-                azure_span_exporter = AzureMonitorTraceExporter(connection_string=connection_string)
-                tracer_provider.add_span_processor(BatchSpanProcessor(azure_span_exporter))
-                set_tracer_provider(tracer_provider)
-                self.tracer = get_tracer_provider().get_tracer(__name__)
+                # Setup tracer provider and exporter
+                tracer_provider = TracerProvider(sampler=ALWAYS_ON, resource=resource)
+                trace.set_tracer_provider(tracer_provider)
+                trace_exporter = AzureMonitorTraceExporter(
+                    connection_string=f"InstrumentationKey={instrumentation_key}"
+                )
+                trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(trace_exporter))
 
-                # Instrument requests library
-                RequestsInstrumentor().instrument(tracer_provider=get_tracer_provider())
-
+                # Set up tracer
+                self.tracer = trace.get_tracer(__name__)
                 self._container_id = config.hostname
                 self.enabled = True
+
             except Exception as ex:
                 self.log_app_insights_exception(ex)
 
@@ -95,11 +89,11 @@ class AppInsightsClient(object):
                 return
             properties = {
                 "custom_dimensions": {
-                    "Request Id": request_id,
-                    "Service Name": config.service_name,
                     "Container Id": self._container_id,
+                    "Request Id": request_id,
                     "Client Request Id": client_request_id,
                     "Workspace Name": config.workspace_name,
+                    "Service Name": config.service_name,
                     "Models": self._model_ids,
                     "Input": json.dumps(model_input),
                     "Prediction": json.dumps(prediction),
@@ -129,6 +123,9 @@ class AppInsightsClient(object):
             except (UnicodeDecodeError, AttributeError) as ex:
                 self.log_app_insights_exception(ex)
                 response_value = "Scoring request response payload is a non serializable object or raw binary"
+                # We have to encode the response value (which is a string) as a JSON to maintain backwards compatibility.
+                # This encodes '{"a": 12}' as '"{\\"a\\": 12}"'
+                response_value = json.dumps(response_value)
         else:
             response_value = None
 
@@ -144,15 +141,15 @@ class AppInsightsClient(object):
                 "url": request.url,
                 "start_time": formatted_start_time,
                 "duration": self._calc_duration(duration_ms),
-                "resultCode": str(response.status_code),
+                "resultCode": str(response.status_code), # Cast to string to maintain backwards compatibility
                 "success": successful,
                 "http_method": request.method,
                 "Workspace Name": config.workspace_name,
                 "Service Name": config.service_name,
             }
 
-            # Send the log to the requests table using OpenTelemetry
-            with self.tracer.start_as_current_span(request.path, kind=SpanKind.SERVER) as span:
+            # Send the log to the requests table
+            with self.tracer.start_as_current_span(request.path, kind=trace.SpanKind.SERVER) as span:
                 for key, value in attributes.items():
                     span.set_attribute(key, value)
         except Exception as ex:
@@ -183,23 +180,32 @@ class AppInsightsClient(object):
             local_duration //= multiplier
         duration_parts.reverse()
         formatted_duration = "%02d:%02d:%02d.%03d" % tuple(duration_parts)
+        if local_duration:
+            formatted_duration = "%d.%s" % (local_duration, formatted_duration)
         return formatted_duration
 
     def _get_model_ids(self):
         # Model information is stored in /var/azureml-app/model_config_map.json in AKS deployments. But, in ACI
         # deployments, that file does not exist due to a bug in container build-out code. Until the bug is fixed
         # /var/azureml-app/azureml-models will be used to enumerate all the models.
+        # For single model setup, config.azureml_model_dir points to /var/azureml-app/azureml-models/$MODEL_NAME/$VERSION
+        # For multiple model setup, it points to the folder containing all deployed models (/var/azureml-app/azureml-models)
         model_ids = []
         try:
             if not config.azureml_model_dir or not os.path.exists(config.azureml_model_dir):
                 logger.warning("Model directory is not set or does not exist: %s", config.azureml_model_dir)
                 return model_ids
-
-            models = [str(model) for model in os.listdir(config.azureml_model_dir)]
-            for model in models:
-                versions = [int(version) for version in os.listdir(os.path.join(config.azureml_model_dir, model))]
-                ids = ["{}:{}".format(model, version) for version in versions]
-                model_ids.extend(ids)
+            elif (os.path.basename(config.azureml_model_dir)).isdigit():
+                model_name = os.path.basename(os.path.dirname(config.azureml_model_dir))
+                model_version = os.path.basename(config.azureml_model_dir)
+                model_ids = ["{}:{}".format(model_name, model_version)]
+                return model_ids
+            else:
+                models = [str(model) for model in os.listdir(config.azureml_model_dir)]
+                for model in models:
+                    versions = [int(version) for version in os.listdir(os.path.join(config.azureml_model_dir, model))]
+                    ids = ["{}:{}".format(model, version) for version in versions]
+                    model_ids.extend(ids)
         except Exception:
             logger.exception("Error while fetching model IDs")
 
